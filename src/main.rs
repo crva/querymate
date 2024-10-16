9d6ff338cc187e883966ea7a922952ac041788f8
crva <cureaux.valentin@gmail.com>
2024-10-16 19:42:23 +0200

    implemented connection and get tables func

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 use config::Config;
 use querymate::postgres;
-use std::collections::HashMap;
 
-fn main() {
-    let settings = Config::builder()
+#[tokio::main]
+async fn main() {
+    connect_to_postgres().await.unwrap()
+}
+
+async fn connect_to_postgres() -> Result<(), Box<dyn std::error::Error>> {
+    let settings: Config = Config::builder()
         .add_source(config::File::with_name("config.toml"))
         .build()
         .unwrap();
 
-    println!(
-        "{:?}",
-        settings
-            .try_deserialize::<HashMap<String, String>>()
-            .unwrap()
-    );
+    let username = settings
+        .clone()
+        .get_string("postgres_username")
+        .unwrap()
+        .to_string();
+    let password = settings
+        .get_string("postgres_password")
+        .unwrap()
+        .to_string();
+    let dbname = settings.get_string("postgres_dbname").unwrap().to_string();
+    let ip = settings.get_string("postgres_ip").unwrap().to_string();
 
-    postgres::connect().unwrap();
+    let postgres_connection = postgres::PostgresConnection {
+        username,
+        password,
+        dbname,
+        ip,
+    };
+
+    let pool = postgres::connect(postgres_connection).await.unwrap();
+    postgres::get_db_tables(&pool).await.unwrap();
+
+    Ok(())
 }
--- src/postgres/mod.rs
+++ src/postgres/mod.rs
@@ -1,14 +1,44 @@
-use sqlx::Row;
+use sqlx::{Pool, Postgres, Row};
 
-#[tokio::main]
-pub async fn connect() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "postgres://postgres:root@localhost/crva";
-    let pool = sqlx::postgres::PgPool::connect(url).await?;
+pub struct PostgresConnection {
+    pub username: String,
+    pub password: String,
+    pub dbname: String,
+    pub ip: String,
+}
 
+impl PostgresConnection {
+    pub fn new(username: String, password: String, dbname: String, ip: String) -> Self {
+        PostgresConnection {
+            username,
+            password,
+            dbname,
+            ip,
+        }
+    }
+}
+
+pub async fn connect(
+    postgres_connection: PostgresConnection,
+) -> Result<Pool<Postgres>, sqlx::Error> {
+    let url = format!(
+        "postgres://{}:{}@{}/{}",
+        postgres_connection.username,
+        postgres_connection.password,
+        postgres_connection.ip,
+        postgres_connection.dbname
+    );
+
+    let pool: Pool<Postgres> = sqlx::postgres::PgPool::connect(&url).await?;
+
+    Ok(pool)
+}
+
+pub async fn get_db_tables(pool: &sqlx::postgres::PgPool) -> Result<(), sqlx::Error> {
     let rows = sqlx::query(
         "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'",
     )
-    .fetch_all(&pool)
+    .fetch_all(pool)
     .await?;
 
     for row in rows {
